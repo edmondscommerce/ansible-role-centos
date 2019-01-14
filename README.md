fabbcf9b40151f81205aadde7b6313a09769109d
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2019-01-14 15:22:42 +0000

    intitial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+Role Name
+=========
+
+Basic Centos setup
+
+Requirements
+------------
+
+n/a
+
+Role Variables
+--------------
+
+n/a
+
+Dependencies
+------------
+
+n/a
+
+Example Playbook
+----------------
+
+Including an example of how to use your role (for instance, with variables passed in as parameters) is always nice for users too:
+
+    - hosts: servers
+      roles:
+         - { role: edmondscommerce.centos }
+
+License
+-------
+
+Apache
+
+Author Information
+------------------
+
+[Edmonds Commerce](https://www.edmondscommerce.co.uk)
---
+++ templates/etc/profile.d/zz_ec.sh
@@ -0,0 +1,86 @@
+# User specific aliases and functions
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias ll='ls -alh --color'
+
+# Make parent dirs if they are missing
+alias mkdir='mkdir -pv'
+# Lets include vim like exit commands
+alias :q="exit"
+alias :Q="exit"
+
+#History
+shopt -s histappend
+shopt -s cmdhist
+HISTCONTROL=ignoredups
+export HISTFILESIZE=20000
+export HISTSIZE=10000
+export HISTIGNORE="&:ls:[bf]g:exit"
+
+
+
+# Settings for interactive shell only inside this block
+if [[ $- == *i* ]]
+then
+
+#    #SSH Agent
+#    if [ -z "$SSH_AUTH_SOCK" ] ; then
+#        inTemp=$(find /tmp -maxdepth 2 -type s -name "agent*" -user $USER -printf '%T@ %p\n' 2>/dev/null |sort -n|tail -1|cut -d' ' -f2)
+#        if [[ "" != "$inTemp" ]]
+#        then
+#            SSH_AUTH_SOCK="$inTemp"
+#            export SSH_AUTH_SOCK
+#        else
+#            eval `ssh-agent -s`
+#            ssh-add
+#        fi
+#    fi
+
+    #Prompt
+    function redPrompt(){
+        export PS1='\[\e[1m\]$PWD\[\e[0m\]'"\n\[\033[38;5;1m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;9m\]\h\[$(tput sgr0)\] "
+    }
+    function bluePrompt(){
+        export PS1='\[\e[1m\]$PWD\[\e[0m\]'"\n\[\033[38;5;32m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;32m\]\h\[$(tput sgr0)\] "
+    }
+    if [[ "$(whoami)" == "root" ]]
+    then
+        redPrompt
+    else
+        bluePrompt
+    fi
+
+    #Prevent Ctrl+S Freezing things
+    stty -ixon
+
+    # fix spelling errors for cd, only in interactive shell
+    shopt -s cdspell
+
+    # More useful bash completelion setting
+    bind "set completion-ignore-case on" # note: bind used instead of sticking these in .inputrc
+    bind "set bell-style none" # no bell
+
+    alias gti=git
+
+    ## To install
+    ## dnf -y install bash-completion
+    [[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && \
+        . /usr/share/bash-completion/bash_completion
+fi
+
+# Moves up a number of dirs
+up(){
+    local d=""
+    limit=$1
+    for ((i=1 ; i <= limit ; i++))
+    do
+        d=$d/..
+    done
+    d=$(echo $d | sed 's/^\///')
+    if [ -z "$d" ]; then
+        d=..
+    fi
+    cd $d
+}
